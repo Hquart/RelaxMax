268791b36d5e83c9bb5b83c717cd75bbb7724c54
Naji Achkar <hquart@icloud.com>
2022-05-20 10:27:27 +0200

    Initial Commit

---
+++ RelaxMax/RelaxMaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelaxMaxApp.swift
+//  RelaxMax
+//
+//  Created by Naji Achkar on 20/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct RelaxMaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
